6b38013ebbea689cbab1ab33f04f9d3ee5cda7ac
Andrew Averill <averilla13@gmail.com>
2017-10-11 16:06:37 -0400

    more updates

--- Plane_Calcs.m
+++ Plane_Calcs.m
@@ -19,18 +19,30 @@ d = 1.225; %kg/m^3 = will be a function of altitude in final flight calc
 A = len*wid; %planform area m^2
 CL = 1.2;%Coefficient of lift
 
-V = sqrt((w*2)/(d*A*CL));%Speed equation
+Lift = w;
+V = sqrt((Lift*2)/(d*A*CL));%Speed equation
 
-Stall_Speed = V*2.23694 %MPH (Top speed between 2.5-3 X stall speed
-Max_Speed = Stall_Speed*3
+Stall_Speed = V*2.23694; %MPH (Top speed between 2.5-3 X stall speed
+Max_Speed = Stall_Speed*3;
 
 %% Drag
 
 cd = .05; %standard drag coefficient of a plane
-Drag = (1+cd)*weight;
+Drag = 244;%cd*A*0.5*d*V^2;
 
-%% Thrust of Prop
-d = 6;
-pitch = 4.5;
-RPMP = (Drag/(4.392399*10^-8)*((d^3.5)/(sqrt(pitch)))*((4.2333*10^-4)*pitch));
-RPM = nthroot(RPMP,4)
+%% Thrust
+
+diam = 20; %prop diameter
+pitch = 8; %prop pitch
+RPM = 10000;
+C1 = 4.392399*10^-8;
+C2 = 4.23333*10^-4;
+
+Thrust = C1*RPM*((diam^3.5)/sqrt(pitch))*(C2*RPM*pitch); %source: http://www.electricrcaircraftguy.com/2013/09/propeller-static-dynamic-thrust-equation.html
+%% DATA
+
+LD_Ratio = Lift/Drag;
+T_ex = Thrust - Drag;%excess thrust
+Acc = T_ex/w;
+Thrust
+%thr = Thrust*101.97
